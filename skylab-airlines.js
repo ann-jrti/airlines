ab0d8e5e5046fc76a79f1a15af1cee0adc4f117d
ann-jrti <asog.jrti@gmail.com>
2021-06-19 19:06:26 +0200

    first commit

---
+++ skylab-airlines.js
@@ -0,0 +1,18 @@
+//greeting function
+const flights = {
+    
+}
+
+
+const greeting = () => {
+    const userName = window.prompt('Welcome to Skyklab Airlines! What is your name?');
+    window.alert(`Hi ${userName}!`)
+}
+
+function airlines () {
+
+    greeting();    
+
+}
+
+airlines();

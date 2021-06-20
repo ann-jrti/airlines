afadc583ef30e2b98a1f68c02c85470094c9adac
ann-jrti <asog.jrti@gmail.com>
2021-06-20 14:55:03 +0200

    fixed wrong files pushed

--- pruebas.js
+++ pruebas.js
@@ -1,4 +1,3 @@
-//flights availables
 const flights = [
 
     { id: 00, to: 'Bilbao', from: 'Barcelona', cost: 1600, scale: false },
@@ -13,27 +12,11 @@ const flights = [
     { id: 09, to: 'Sydney', from: 'Barcelona', cost: 150, scale: true },
     { id: 10, to: 'Tel-Aviv', from: 'Madrid', cost: 150, scale: false } ];
 
-/* side functions */
+//checks if the flights cointains scales  
 
-//greets the user
-const sayHi = () => {
-    const userName = window.prompt('Welcome to Skyklab Airlines! What is your name?');
-    window.alert(`Hi ${userName}!`)
-}
-
-//checks if flights have scales  
-const scaleChecker = (arr) => {
+const scaleFlightChecker = (arr) => {
     for (const element of arr) {
-        if (element.scale) {
-            return true
-        } else return false
-    }
-}
-
-//show flights in a friendly format
-const showFlights = (arr) => {
-    for (const element of arr) {
-        if (scaleChecker(flights)) {
+        if (element.scale === true) {
             console.log(`The flight with origin: ${element.from}, and destination: ${element.to}, has a cost of ${element.cost}€ and has at least 1 scale.`)
     } else {
         console.log(`The flight with origin: ${element.from}, and destination ${element.to}, has a cost of ${element.cost}€ and has no scales.`)
@@ -41,50 +24,30 @@ const showFlights = (arr) => {
     }
 }
 
-//calculates and shows medium cost of total flights
-const flightsAveragePrice = (arr) => {
+const flightMediumCost = (arr) => {
     let totalCost = 0
-    let averagePrice 
+    let mediumCost 
     for (const element of arr) {
         totalCost += element.cost
     }
-    averagePrice = totalCost / 10;
-    console.log(`The average price of total flights is ${averagePrice}€`)
-}
-
-//shows how many flights has scales
-const showFlightsWithScale = (arr) => {
-    const flightsWithScale = []
-    for (const element of arr) {
-        if (element.scale) {
-            flightsWithScale.push(element.to)
-        }
-    }
-    const numberOfFlightsWithWScale = flightsWithScale.length
-    console.log(`Today we have ${numberOfFlightsWithWScale} fligths with scale to: ${flightsWithScale}`); 
-    
-}
-
-//shows to user the five last flights of the day
-const showsLastFlightsOfTheDay = (arr) => {
-    let lastFiveFlights = []
-        for (let i = 0; i < arr.length; i++) {
-            let lastFive = arr.length - 5
-            if (i >= lastFive) {
-                lastFiveFlights.push(arr[i].to)
-            }
-        }
-    console.log(`The last flights are ${lastFiveFlights}`)
+    mediumCost = totalCost / 10;
+    console.log(mediumCost)
 }
     
-/* main function */
+//greets the user
+const greeting = () => {
+    const userName = window.prompt('Welcome to Skyklab Airlines! What is your name?');
+    window.alert(`Hi ${userName}!`)
+}
 
 function airlines () {
-    //sayHi();
-    showFlights(flights);
-    flightsAveragePrice(flights);
-    showFlightsWithScale(flights);
-    showsLastFlightsOfTheDay(flights);
+
+    //greeting();
+    scaleFlightChecker(flights);
+    flightMediumCost(flights);
+  
+
 }
 
 airlines();
+
